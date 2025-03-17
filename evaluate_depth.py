e71dee236b2313470e2904570f5d46fcdbee01d1
jcordero26 <javiercordero1296@gmail.com>
2025-03-16 19:14:08 -0600

    Integrate as submodule in yolo

--- evaluate_depth.py
+++ evaluate_depth.py
@@ -7,11 +7,11 @@ import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
-from layers import disp_to_depth
-from utils import readlines
-from options import MonodepthOptions
-import datasets
-import networks
+from .layers import disp_to_depth
+from .utils import readlines
+from .options import MonodepthOptions
+from . import datasets
+from . import networks
 
 cv2.setNumThreads(0)  # This speeds up evaluation 5x on our unix systems (OpenCV 3.3.1)
 
